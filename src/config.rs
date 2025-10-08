cd24a270f0cd8dc452903f394487062b6e17f7b3
choas-ci <yuguokksk@gmail.com>
2025-10-08 13:53:03 +0800

    1.4.2

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.betonm.cn"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.200.11"];
 pub const RS_PUB_KEY: &str = "csbc@123";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
